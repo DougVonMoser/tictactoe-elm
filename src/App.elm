c79332e09ed0168a304b71243acfe9650436d25d
Doug VonMoser <dvonmoser@cars.com>
2018-06-26 09:20:49 -0500

    line one

--- src/App.elm
+++ src/App.elm
@@ -5,13 +5,22 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 
+type Marked = Blank | X | O
+
 type alias Model =
-    Int
+    List Marked
+    -- List (List Marked)
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( 0, Cmd.none )
+    ( 
+        -- [
+        [Blank, Blank, Blank]
+        -- , [Blank, Blank, Blank]
+        -- , [Blank, Blank, Blank]
+    -- ]
+    , Cmd.none )
 
 
 
@@ -26,17 +35,7 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
         Inc ->
-            ( add1 model, Cmd.none )
-
-
-{-| increments the counter
-
-    add1 5 --> 6
-
--}
-add1 : Model -> Model
-add1 model =
-    model + 1
+            ( model, Cmd.none )
 
 
 
@@ -44,24 +43,17 @@ add1 model =
 
 
 view : Model -> Html Msg
-view model =
-    div [ class "container" ]
-        [ header []
-            [ img [ src "images/logo.png" ] []
-            , h1 [] [ text "Elm Webpack Starter, featuring hot-loading" ]
-            ]
-        , p [] [ text "Click on the button below to increment the state." ]
-        , div []
-            [ button
-                [ class "pure-button pure-button-primary"
-                , onClick Inc
-                ]
-                [ text "+ 1" ]
-            , text <| toString model
-            ]
-        , p [] [ text "Then make a change to the source code and see how the state is retained after you recompile." ]
-        , p []
-            [ text "And now don't forget to add a star to the Github repo "
-            , a [ href "https://github.com/simonh1000/elm-webpack-starter" ] [ text "elm-webpack-starter" ]
-            ]
-        ]
+view model = 
+    div [] (List.map viewMarked model)
+
+
+
+viewMarked : Marked -> Html Msg
+viewMarked marked =
+    case marked of
+        Blank ->
+            text "Blanco"
+        X ->
+            text "X"
+        O -> 
+            text "O"
